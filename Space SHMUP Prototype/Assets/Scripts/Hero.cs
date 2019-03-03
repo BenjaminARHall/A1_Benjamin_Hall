09519b8c6caf28d28e6ecd76aa0ec6b5fe3cda23
BenjaminARHall <b-a-hall@hotmail.com>
2019-03-03 16:23:59 +1100

    Added Power up
    Added power up and extra weapon slots.

--- Space SHMUP Prototype/Assets/Scripts/Hero.cs
+++ Space SHMUP Prototype/Assets/Scripts/Hero.cs
@@ -14,12 +14,23 @@ public class Hero : MonoBehaviour
     // Ship status information
     [SerializeField]
     private float _shieldLevel = 1;
+    // Weapon fields
+    public Weapon[] weapons;
     public bool ____________________________;
     public Bounds bounds;
+
+    // Declare a new delegate type WeaponFireDelegate
+    public delegate void WeaponFireDelegate();
+    // Create a WeaponFireDelegate field named fireDelegate.
+    public WeaponFireDelegate fireDelegate;
+
     void Awake()
     {
         S = this; // Sets the Singleton
         bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+        // Reset the weapons to start _Hero with 1 blaster
+        ClearWeapons();
+        weapons[0].SetType(WeaponType.blaster);
     }
     void Update()
     {
@@ -45,6 +56,14 @@ public class Hero : MonoBehaviour
         }
         // Rotate the ship to make it feel more dynamic // 2
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+        // Use the fireDelegate to fire Weapons
+        // First, make sure the Axis("Jump") button is pressed
+        // Then ensure that fireDelegate isn't null to avoid an error
+        if (Input.GetAxis("Jump") == 1 && fireDelegate != null)
+        { // 1
+            fireDelegate();
+        }
     }
     public GameObject lastTriggerGo = null;
     void OnTriggerEnter(Collider other)
@@ -67,13 +86,19 @@ public class Hero : MonoBehaviour
                 // Decrease the level of the shield by 1
                 shieldLevel--;
                 // Destroy the enemy
-                Destroy(go); // 4
+                Destroy(go); // 4 
+                print("Triggered: " + go.name); // Move this line here!
+            }
+            else if (go.tag == "PowerUp")
+            {
+                // If the shield was triggerd by a PowerUp
+                AbsorbPowerUp(go);
             }
             else
             {
-                print("Triggered: " + go.name); // Move this line here!
+               print("Triggered: " + other.gameObject.name);
             }
-            print("Triggered: " + other.gameObject.name);
+            
         }
     }
     public float shieldLevel
@@ -92,6 +117,54 @@ public class Hero : MonoBehaviour
                 // Tell Main.S to restart the game after a delay
                 Main.S.DelayedRestart(gameRestartDelay);
             }
+        }
+    }
+    public void AbsorbPowerUp(GameObject go)
+    {
+        PowerUp pu = go.GetComponent<PowerUp>();
+        switch (pu.type)
+        {
+            case WeaponType.shield: // If it's the shield
+                shieldLevel++;
+                break;
+            default: // If it's any Weapon PowerUp
+                     // Check the current weapon type
+                if (pu.type == weapons[0].type)
+                {
+                    // then increase the number of weapons of this type
+                    Weapon w = GetEmptyWeaponSlot(); // Find an available weapon
+                    if (w != null)
+                    {
+                        // Set it to pu.type
+                        w.SetType(pu.type);
+                    }
+                }
+                else
+                {
+                    // If this is a different weapon
+                    ClearWeapons();
+                    weapons[0].SetType(pu.type);
+                }
+                break;
+        }
+        pu.AbsorbedBy(this.gameObject);
+    }
+    Weapon GetEmptyWeaponSlot()
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            if (weapons[i].type == WeaponType.none)
+            {
+                return (weapons[i]);
+            }
+        }
+        return (null);
+    }
+    void ClearWeapons()
+    {
+        foreach (Weapon w in weapons)
+        {
+            w.SetType(WeaponType.none);
         }
     }
 }
